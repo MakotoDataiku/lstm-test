050111d32ceacf9ad54588c1f7708a51c939b161
admin <makoto.miyazaki@dataiku.com>
2021-01-12 13:49:56 +0100

    edit files

---
+++ lib/python/custom_MeanClassifier/__init__.py
@@ -0,0 +1,10 @@
+def __init__(self, arg1, arg2, arg3, ..., argN):
+
+    # print("Initializing classifier:\n")
+
+    args, _, _, values = inspect.getargvalues(inspect.currentframe())
+    values.pop("self")
+
+    for arg, val in values.items():
+        setattr(self, arg, val)
+        # print("{} = {}".format(arg,val)

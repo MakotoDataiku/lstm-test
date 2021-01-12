4eeacebd9153c72c850bfb2754f38f357c6f4211
admin <makoto.miyazaki@dataiku.com>
2021-01-12 17:28:19 +0100

    Recipe CUSTOMMODELS.compute_output_scored_2 saved through notebook notebook editor for compute_output_scored_2

--- recipes/compute_output_scored_2.py
+++ recipes/compute_output_scored_2.py
@@ -1,22 +1,38 @@
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # -*- coding: utf-8 -*-
 import dataiku
 import pandas as pd, numpy as np
 from dataiku import pandasutils as pdu
+import pickle
 
 # Read recipe inputs
 models = dataiku.Folder("DQ1im0k6")
 models_info = models.get_info()
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+model_path = models.file_path("finalized_model.sav")
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 output = dataiku.Dataset("output")
-output_df = output.get_dataframe()
+df = output.get_dataframe()
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+# load the model from disk
+loaded_model = pickle.load(open(model_path, 'rb'))
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+X_test = df[["bathrooms", "bedrooms", "sqft_living"]].values
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+result = loaded_model.predict(X_test)
+df_test["predicted"] = result
 
 
 # Compute recipe outputs from inputs
 # TODO: Replace this part by your actual code that computes the output, as a Pandas dataframe
 # NB: DSS also supports other kinds of APIs for reading and writing data. Please see doc.
 
-output_scored_2_df = output_df # For this sample code, simply copy input to output
-
 
 # Write recipe outputs
 output_scored_2 = dataiku.Dataset("output_scored_2")
-output_scored_2.write_with_schema(output_scored_2_df)
+output_scored_2.write_with_schema(df_test)

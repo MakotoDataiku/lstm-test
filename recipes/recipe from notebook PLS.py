99bd07c9ba171134053560f4d007adcb5ff04e70
admin <makoto.miyazaki@dataiku.com>
2021-01-12 15:40:20 +0100

    Saved recipe: CUSTOMMODELS.recipe from notebook PLS

--- recipes/recipe from notebook PLS.py
+++ recipes/recipe from notebook PLS.py
@@ -4,11 +4,15 @@ from sklearn.model_selection import train_test_split
 from custom_PLS.custom_PLS import PLS
 
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
-my_dataset = dataiku.Dataset("house_price")
-df = my_dataset.get_dataframe()
-X = df[["bathrooms", "bedrooms", "sqft_living"]].values
-y = df["price"].values
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
+df_train = dataiku.Dataset("house_price").get_dataframe()
+df_test = dataiku.Dataset("output").get_dataframe()
+
+
+X_train = df_train[["bathrooms", "bedrooms", "sqft_living"]].values
+X_test = df_test[["bathrooms", "bedrooms", "sqft_living"]].values
+y_train = df_train["price"].values
+y_test = df_test["price"].values
+
 
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 reg = PLS(n_components=2)
@@ -17,15 +21,16 @@ reg = PLS(n_components=2)
 reg.fit(X_train, y_train)
 
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
-reg.predict(X_test)
+y_predicted = reg.predict(X_test)
+y_proba = reg.predict_proba(X_test)
 
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 reg.score(X_test, y_test)
 
-# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
-reg.get_params()
+df_test["predicted"] = y_predicted
+df_test["proba"] = y_proba
 
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # Recipe outputs
 output_scored = dataiku.Dataset("output_scored")
-output_scored.write_with_schema(pandas_dataframe)
+output_scored.write_with_schema(df_test)

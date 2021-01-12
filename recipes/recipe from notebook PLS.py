97e229c251432eaef648e3812a7ff8a8278f1b7a
admin <makoto.miyazaki@dataiku.com>
2021-01-12 17:40:30 +0100

    Saved recipe: CUSTOMMODELS.recipe from notebook PLS

--- recipes/recipe from notebook PLS.py
+++ recipes/recipe from notebook PLS.py
@@ -3,27 +3,17 @@ import dataiku
 from sklearn.model_selection import train_test_split
 from custom_PLS.custom_PLS import PLS
 
-# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 df_train = dataiku.Dataset("house_price").get_dataframe()
 df_test = dataiku.Dataset("output").get_dataframe()
-
 
 X_train = df_train[["bathrooms", "bedrooms", "sqft_living"]].values
 X_test = df_test[["bathrooms", "bedrooms", "sqft_living"]].values
 y_train = df_train["price"].values
 y_test = df_test["price"].values
 
-
-# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 reg = PLS(n_components=2)
-
-# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 reg.fit(X_train, y_train)
-
-# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 y_predicted = reg.predict(X_test)
-
-# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 reg.score(X_test, y_test)
 
 df_test["predicted"] = y_predicted

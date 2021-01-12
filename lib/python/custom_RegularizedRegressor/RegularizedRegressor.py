a6569d3f09e63e22aa6801733e6568ee6a475dcb
admin <makoto.miyazaki@dataiku.com>
2021-01-12 14:03:03 +0100

    edit files

---
+++ lib/python/custom_RegularizedRegressor/RegularizedRegressor.py
@@ -0,0 +1,26 @@
+import numpy as np
+from sklearn.cross_validation import cross_val_score
+
+class RegularizedRegressor:
+    def __init__(self, l = 0.01):
+        self.l = l
+
+    def combine(self, inputs):
+        return sum([i*w for (i,w) in zip([1] + inputs, self.weights)])
+
+    def predict(self, X):
+        return [self.combine(x) for x in X]
+
+    def classify(self, inputs):
+        return sign(self.predict(inputs))
+
+    def fit(self, X, y, **kwargs):
+        self.l = kwargs['l']
+        X = np.matrix(X)
+        y = np.matrix(y)
+        W = (X.transpose() * X).getI() * X.transpose() * y
+
+        self.weights = [w[0] for w in W.tolist()]
+
+    def get_params(self, deep = False):
+        return {'l':self.l}
---
+++ lib/python/custom_RegularizedRegressor/__init__.py
@@ -0,0 +1,1 @@
+from custom_RegularizedRegressor.RegularizedRegressor import RegularizedRegressor

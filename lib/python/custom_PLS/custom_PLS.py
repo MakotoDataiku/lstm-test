7b3b71c906bbcee2752cdde72cb59b0e54dba9f6
admin <makoto.miyazaki@dataiku.com>
2021-01-12 14:14:42 +0100

    edit files

---
+++ lib/python/custom_PLS/custom_PLS.py
@@ -0,0 +1,28 @@
+from sklearn.cross_decomposition import PLSRegression
+
+class ExamplePLS():
+    def __init__(self, n_components=2, scale=True, max_iter=500, tol=1e-06, copy=True):
+        self.pls = PLSRegression(n_components, scale, max_iter, tol, copy)
+        
+    
+    def fit(self, X, Y):
+        self.pls.fit(X, Y)
+        return self.pls
+    
+    def predict(self, X, Y, copy=True):
+        return self.pls.predict(X, copy).flatten()
+    
+    def score(self, X, Y, sample_weight=None):
+        return self.pls.score(X, Y, sample_weight)
+    
+    def get_params(self, **parameters):
+        self.pls.set_params(**parameters)
+        return self
+    
+    @property
+    def intercept_(self):
+        return 0
+    
+    @property
+    def coeff_(self):
+        return self.pls.coef_
